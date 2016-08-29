353f38e8f11dc0b45de53fdd3d88ae36136602bb
Brian Bamsch <bbamsch32@gmail.com>
2016-08-28 20:14:19 -0700

    Euler Problem 9 - Unit Test

---
+++ euler/src/test/java/Problem9Test.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Brian on 8/28/2016.
+ */
+public class Problem9Test {
+    @Test
+    public void specialPythagoreanTriplet() throws Exception {
+        Triplet<Integer> pythagoreanTriplet = Problem9.specialPythagoreanTriplet(1000);
+        Assert.assertEquals(Integer.valueOf(3), pythagoreanTriplet.getA());
+        Assert.assertEquals(Integer.valueOf(4), pythagoreanTriplet.getB());
+        Assert.assertEquals(Integer.valueOf(5), pythagoreanTriplet.getC());
+    }
+}
