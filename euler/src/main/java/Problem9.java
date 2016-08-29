825cf0f55db00499a707f08a6533866023745f32
Brian Bamsch <bbamsch32@gmail.com>
2016-08-28 21:01:11 -0700

    Euler Problem 9 - Simple Implementation for Special Pythagorean Triplet

--- euler/src/main/java/Problem9.java
+++ euler/src/main/java/Problem9.java
@@ -33,7 +33,29 @@ public class Problem9 {
         }
     }
 
+    /**
+     * Pythagorean Triplet Calculation
+     * <p>
+     * We only deal with two variables as x is given and the last variable is
+     * implied by the values of the first two.
+     * <p>
+     * Straightforward solution is to scan through all values of a and b to find
+     * a solution which satisfies both requirements (a^2 + b^2 = c^2 & a + b + c = x)
+     *
+     * @param x the sum of a + b + c for the pythagorean triplet
+     * @return a triplet (a, b, c) which sums to x following pythagorean theorem
+     */
     public static Triplet<Integer> specialPythagoreanTriplet(int x) {
+        // Upper constraint of a is x/3 as a < b < c and a + b + c = x
+        for (int a = 1; a < x / 3; a++) {
+            // Upper constraint of x/2 as b < c and a + b + c = x and a > 1
+            for (int b = a + 1; b < x / 2; b++) {
+                int c = x - a - b;
+                if ((a * a) + (b * b) == (c * c)) {
+                    return new Triplet<>(a, b, c);
+                }
+            }
+        }
         return null;
     }
 }
