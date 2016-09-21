b7e5773fcb3f0e9055e0290fd61b219b604c9ce1
Brian Bamsch <bbamsch32@gmail.com>
2016-09-20 19:49:44 -0700

    Add Minimum Spanning Set Problem

---
+++ other/src/main/java/MinimumSpanningSet.java
@@ -0,0 +1,17 @@
+import javafx.util.Pair;
+
+/**
+ * Minimum Spanning Set
+ * --------------------
+ * Given a list of ranges (inclusive), find the minimum set of ranges which span the entirety of the input set.
+ * For example, [(0,4),(5,6),(4,5),(7,10)] -> [(0,6),(7,10)]
+ */
+public class MinimumSpanningSet {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Pair<Integer>> minimumSpanningSet(List<Pair<Integer>> inputRanges) {
+
+    }
+}
