72cda4ce005f13f71c198d2521ec3a8d9ed8b6ad
Brian Bamsch <bbamsch32@gmail.com>
2016-09-21 22:24:02 -0700

    Add Test for SumToTarget Problem

--- careercup/src/test/java/SumToTargetTest.java
+++ careercup/src/test/java/SumToTargetTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -7,6 +8,13 @@ import org.junit.Test;
 public class SumToTargetTest {
     @Test
     public void sumToTarget() throws Exception {
-        
+        long[] list1 = new long[]{1L, 5L, 298L, 27L, 11L};
+        long[] list2 = new long[]{492L, 28L, 5L, 17L};
+
+        Assert.assertTrue(SumToTarget.sumToTarget(list1, list2, 10L));
+        Assert.assertTrue(SumToTarget.sumToTarget(list1, list2, 790L));
+        Assert.assertTrue(SumToTarget.sumToTarget(list1, list2, 18L));
+        Assert.assertFalse(SumToTarget.sumToTarget(list1, list2, 492L));
+        Assert.assertFalse(SumToTarget.sumToTarget(list1, list2, 753L));
     }
 }
