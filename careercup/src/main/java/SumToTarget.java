407727054eaa924fbbac3e71d64f1ca88d11d149
Brian Bamsch <bbamsch32@gmail.com>
2016-09-21 22:26:56 -0700

    Add DocString to SumToTarget

--- careercup/src/main/java/SumToTarget.java
+++ careercup/src/main/java/SumToTarget.java
@@ -10,14 +10,25 @@ import java.util.Set;
  * Return true if so, else return false.
  */
 public class SumToTarget {
+    /**
+     * Searches if a pair of elements (one from list1, one from list2) adds to the target value.
+     * <p>
+     * Runtime: O(n)
+     * Space: O(n)
+     *
+     * @param list1  list of elements
+     * @param list2  list of elements
+     * @param target target sum value
+     * @return true if pair exists, false otherwise
+     */
     public static boolean sumToTarget(long[] list1, long[] list2, long target) {
-        // Add all elems of list2 to a Set -- O(n)
+        // Add all elems of list2 to a Set
         Set<Long> list2Set = new HashSet<>();
         for (long elem : list2) {
             list2Set.add(elem);
         }
 
-        // Iterate through list1, check for matching pair in list2 -- O(n)
+        // Iterate through list1, check for matching pair in list2
         for (long elem : list1) {
             if (list2Set.contains(target - elem)) {
                 return true;
