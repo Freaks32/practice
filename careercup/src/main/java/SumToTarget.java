099391a08537e0ff06d5ec111d871ecefe5cfeef
Brian Bamsch <bbamsch32@gmail.com>
2016-09-21 22:16:27 -0700

    Add Skeleton for SumToTarget

---
+++ careercup/src/main/java/SumToTarget.java
@@ -0,0 +1,13 @@
+/**
+ * Solution for https://careercup.com/question?id=5739394251096064
+ * --
+ * Given two integer arrays list1 and list2 and an int target value.
+ * WAP to check if there exists such a sum, where one number taken
+ * from list1 and other from list2 to add up to become the target value.
+ * Return true if so, else return false.
+ */
+public class SumToTarget {
+    public static boolean sumToTarget(long[] list1, long[] list2, long target) {
+        
+    }
+}
---
+++ careercup/src/test/java/SumToTargetTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Tests for SumToTarget Problem
+ * https://careercup.com/question?id=5739394251096064
+ */
+public class SumToTargetTest {
+    @Test
+    public void sumToTarget() throws Exception {
+        
+    }
+}
