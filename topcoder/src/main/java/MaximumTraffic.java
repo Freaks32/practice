1401802cc92b93e703077f5f0aef24cd78df8a90
Brian Bamsch <bbamsch32@gmail.com>
2016-09-22 23:56:23 -0700

    MaximumTraffic: Add DFS Sum to calculate sum of graph branch

--- topcoder/src/main/java/MaximumTraffic.java
+++ topcoder/src/main/java/MaximumTraffic.java
@@ -19,6 +19,22 @@ public class MaximumTraffic {
 
     public static List<Pair<Long, Long>> maximumTraffic(List<GraphNode<Long>> graph) {
         return null;
+
+    /**
+     * Does a depth first search on a section of graph to calculate sum of all components (with backtracking to parent)
+     *
+     * @param node the node to act as the root of the dfs
+     * @param parentKey key of the parent to prevent backtracking
+     * @return the sum of all populations on the branch
+     */
+    private Long dfsSum(GraphNode<Long> node, Long parentKey) {
+        Long treeTotal = node.value;
+        for (Long neighborKey : node.neighbors) {
+            if (!neighborKey.equals(parentKey)) {
+                treeTotal += dfsSum(getNode(neighborKey), node.value);
+            }
+        }
+        return treeTotal;
     }
 
     private GraphNode<Long> getNode(Long key) {
