ea7a8160de25d3d1b76f18ca42d9d977fe0dc0b4
Brian Bamsch <bbamsch32@gmail.com>
2016-09-23 22:43:37 -0700

    FibonacciDiv2: Add fibonacciLookup for storage of fibonacci sequence

--- topcoder/src/main/java/FibonacciDiv2.java
+++ topcoder/src/main/java/FibonacciDiv2.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * FibonacciDiv2 Problem from TopCoder
  * https://community.topcoder.com/stat?c=problem_statement&pm=13159
  */
 public class FibonacciDiv2 {
+    List<Integer> fibonacciLookup = new ArrayList<>();
+
+    public FibonacciDiv2() {
+        fibonacciLookup.add(0);
+        fibonacciLookup.add(1);
+    }
+
     public int find(int n) {
         return 0;
     }
