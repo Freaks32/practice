455b4006cd5bd939fb20cd3bf4696f67efdd4434
Brian Bamsch <bbamsch32@gmail.com>
2016-09-23 22:42:48 -0700

    FibonacciDiv2: Add Skeleton

---
+++ topcoder/src/main/java/FibonacciDiv2.java
@@ -0,0 +1,9 @@
+/**
+ * FibonacciDiv2 Problem from TopCoder
+ * https://community.topcoder.com/stat?c=problem_statement&pm=13159
+ */
+public class FibonacciDiv2 {
+    public int find(int n) {
+        return 0;
+    }
+}
