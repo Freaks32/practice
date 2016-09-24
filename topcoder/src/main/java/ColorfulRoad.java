dbb999dbe73653bd5a041a201effc9177adea9b3
Brian Bamsch <bbamsch32@gmail.com>
2016-09-23 22:02:00 -0700

    Skeleton for ColorfulRoad Problem

---
+++ topcoder/src/main/java/ColorfulRoad.java
@@ -0,0 +1,9 @@
+/**
+ * ColorfulROad Problem from TopCoder
+ * https://community.topcoder.com/stat?c=problem_statement&pm=12837
+ */
+public class ColorfulRoad {
+    public int getMin(String road) {
+        
+    }
+}
