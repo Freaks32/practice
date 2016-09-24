bdf6dc402c0fdd6533f4f9552b88b518cf978d3e
Brian Bamsch <bbamsch32@gmail.com>
2016-09-23 21:21:20 -0700

    Skeleton for ShorterSuperSum

---
+++ topcoder/src/main/java/ShorterSuperSum.java
@@ -0,0 +1,9 @@
+/**
+ * ShorterSuperSum Problem from TopCoder
+ * https://community.topcoder.com/stat?c=problem_statement&pm=10240
+ */
+public class ShorterSuperSum {
+    public static int calculate(int k, int n) {
+        return 0;
+    }
+}
