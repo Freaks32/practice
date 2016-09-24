824087bd9b126ebb01a48bf281ce7347ed7fa487
Brian Bamsch <bbamsch32@gmail.com>
2016-09-23 22:14:12 -0700

    ColorfulRoad: Add nextColor helper function

--- topcoder/src/main/java/ColorfulRoad.java
+++ topcoder/src/main/java/ColorfulRoad.java
@@ -6,4 +6,17 @@ public class ColorfulRoad {
     public int getMin(String road) {
         
     }
+
+    private static char nextColor(char currentColor) {
+        switch (currentColor) {
+            case 'R':
+                return 'G';
+            case 'G':
+                return 'B';
+            case 'B':
+                return 'R';
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
 }
