9e8ce1d43521b7c4b011636a4b8437f33c4915ff
Brian Bamsch <bbamsch32@gmail.com>
2016-09-24 18:28:11 -0700

    PrimeSoccer: Add PrimeSet for quick primality lookup
    For this problem, soccer will only have 18 5-minute intervals with
    assumed max score of 1 per interval. We will only need prime numbers up
    to 18 here.

--- topcoder/src/main/java/PrimeSoccer.java
+++ topcoder/src/main/java/PrimeSoccer.java
@@ -1,9 +1,15 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * PrimeSoccer Problem from TopCoder
  * https://community.topcoder.com/stat?c=problem_statement&pm=10033
  */
 public class PrimeSoccer {
+    // Only need Primes up to 18 as Soccer will only ever be 18 rounds w/ no more than 1 score each round
+    private static Set<Integer> primeSet = new HashSet<>(Arrays.asList(2, 3, 5, 7, 9, 11, 13, 17));
+
     public double getProbability(int skillOfTeamA, int skillOfTeamB) {
         return 0D;
     }
