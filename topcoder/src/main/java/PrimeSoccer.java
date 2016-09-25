6c3127f650a87af6664511c40a284cbae861222e
Brian Bamsch <bbamsch32@gmail.com>
2016-09-24 18:34:20 -0700

    PrimeSoccer: Implement base functionality
    Define helper function to do calculation of probability of team scoring
    prime value. Implement base function to utilize this helper function

--- topcoder/src/main/java/PrimeSoccer.java
+++ topcoder/src/main/java/PrimeSoccer.java
@@ -11,6 +11,20 @@ public class PrimeSoccer {
     private static Set<Integer> primeSet = new HashSet<>(Arrays.asList(2, 3, 5, 7, 9, 11, 13, 17));
 
     public double getProbability(int skillOfTeamA, int skillOfTeamB) {
+        // Probabilities of each team scoring per round
+        double probA = (double) skillOfTeamA / 100D;
+        double probB = (double) skillOfTeamB / 100D;
+
+        // Probabilities of each team scoring a prime value
+        double probPrimeA = getProbabilityRounds(0, probA, 18);
+        double probPrimeB = getProbabilityRounds(0, probB, 18);
+
+        // Probability is the inverse of the chances of Team A not scoring
+        // prime multiplied by chances of Team B not scoring prime
+        return 1D - ((1D - probPrimeA) * (1D - probPrimeB));
+    }
+
+    private double getProbabilityRounds(int currentScore, double probability, int roundsLeft) {
         return 0D;
     }
 }
