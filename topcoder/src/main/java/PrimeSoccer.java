a6b46d47e0d559075ca46a4a732c3d515697f775
Brian Bamsch <bbamsch32@gmail.com>
2016-09-24 18:20:47 -0700

    PrimeSoccer: Add Skeleton

---
+++ topcoder/src/main/java/PrimeSoccer.java
@@ -0,0 +1,10 @@
+
+/**
+ * PrimeSoccer Problem from TopCoder
+ * https://community.topcoder.com/stat?c=problem_statement&pm=10033
+ */
+public class PrimeSoccer {
+    public double getProbability(int skillOfTeamA, int skillOfTeamB) {
+        return 0D;
+    }
+}
