e0fd23c8e1a7fef8ea03cd22871f73446971eda5
Brian Bamsch <bbamsch32@gmail.com>
2016-09-24 20:48:27 -0700

    UnsealTheSafe: Add caching for combinations
    Cache combination by number of keys presses remaining and last key
    pressed

--- topcoder/src/main/java/UnsealTheSafe.java
+++ topcoder/src/main/java/UnsealTheSafe.java
@@ -3,7 +3,29 @@
  * https://community.topcoder.com/stat?c=problem_statement&pm=4471
  */
 public class UnsealTheSafe {
+    private static long NO_CACHED_VALUE = -1L;
+
+    long[][] comboLookup;
+
     public long countPasswords(int N) {
+        initComboLookup(N);
         return 0L;
+    }
+
+    private void initComboLookup(int N) {
+        comboLookup = new long[N][10];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 10; j++) {
+                comboLookup[i][j] = NO_CACHED_VALUE;
+            }
+        }
+    }
+
+    long getCachedCombos(int N, int key) {
+        return comboLookup[N - 1][key];
+    }
+
+    void setCachedCombos(int N, int key, long combos) {
+        comboLookup[N - 1][key] = combos;
     }
 }
