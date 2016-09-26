f5478612b2035fb33a4efd78f5ff529105716910
Brian Bamsch <bbamsch32@gmail.com>
2016-09-25 17:53:20 -0700

    SRMCards: Add cardsInPlay Set
    Add set to keep track of cards currently in play

--- topcoder/src/main/java/SRMCards.java
+++ topcoder/src/main/java/SRMCards.java
@@ -1,10 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * SRMCards Problem from TopCoder
  * https://community.topcoder.com/stat?c=problem_statement&pm=11341
  */
 public class SRMCards {
+    Set<Integer> cardsInPlay;
+
     public int maxTurns(int[] cards) {
+        cardsInPlay = new HashSet<>();
+        for (int card : cards) {
+            cardsInPlay.add(card);
+        }
         return 0;
     }
+
+    private boolean cardInPlay(int card) {
+        return cardsInPlay.contains(card);
+    }
+
+    private void removeCard(int card) {
+        if (cardInPlay(card)) {
+            cardsInPlay.remove(card);
+        }
     }
 }
