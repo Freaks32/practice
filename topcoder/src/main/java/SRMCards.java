220b97093fbc46105262bc777106a1d9f1df7ddd
Brian Bamsch <bbamsch32@gmail.com>
2016-09-25 17:51:15 -0700

    SRMCards: Add Skeleton

---
+++ topcoder/src/main/java/SRMCards.java
@@ -0,0 +1,9 @@
+/**
+ * SRMCards Problem from TopCoder
+ * https://community.topcoder.com/stat?c=problem_statement&pm=11341
+ */
+public class SRMCards {
+    public int maxTurns(int[] cards) {
+        return 0;
+    }
+}
