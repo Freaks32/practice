0483617596f7feec035c5a1645c89d8698069372
Brian Bamsch <bbamsch32@gmail.com>
2016-09-25 20:57:07 -0700

    BinaryTreeLists: Finalize Test

--- other/src/test/java/BinaryTreeListsTest.java
+++ other/src/test/java/BinaryTreeListsTest.java
@@ -1,4 +1,8 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Tests for the BinaryTreeLists problem
@@ -10,10 +14,14 @@ public class BinaryTreeListsTest {
         bst.add(5);
         bst.add(1);
         bst.add(8);
-        bst.add(7);
         bst.add(6);
-        bst.add(10);
 
-        BinaryTreeLists.getPossibleLists(bst);
+        List<List<Integer>> expectedResult = Arrays.asList(
+                Arrays.asList(5, 1, 8, 6),
+                Arrays.asList(5, 8, 1, 6),
+                Arrays.asList(5, 8, 6, 1)
+        );
+
+        Assert.assertEquals(expectedResult, BinaryTreeLists.getPossibleLists(bst));
     }
 }
