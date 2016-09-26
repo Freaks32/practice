0c9c2afdab054f42821fc98b88f8a91c1e8192f8
Brian Bamsch <bbamsch32@gmail.com>
2016-09-25 19:39:23 -0700

    BinaryTreeLists: Encase Node in BST Class

--- other/src/main/java/BinaryTreeLists.java
+++ other/src/main/java/BinaryTreeLists.java
@@ -11,30 +11,58 @@ public class BinaryTreeLists {
     }
 }
 
-class Node<T> {
-    T value;
-    Node<T> left;
-    Node<T> right;
+class BST<T extends Comparable> {
+    Node<T> root;
 
-    public Node(T value) {
-        this(value, null, null);
+    public void add(T value) {
+        if (root != null) {
+            root.add(value);
+        } else {
+            root = new Node<>(value);
+        }
     }
 
-    public Node(T value, Node<T> left, Node<T> right) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
+    class Node<T extends Comparable> {
+        T value;
+        Node<T> left;
+        Node<T> right;
 
-    public T getValue() {
-        return value;
-    }
+        public Node(T value) {
+            this(value, null, null);
+        }
 
-    public Node<T> getLeftChild() {
-        return left;
-    }
+        public Node(T value, Node<T> left, Node<T> right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
 
-    public Node<T> getRightChild() {
-        return right;
+        public void add(T value) {
+            if (value.compareTo(this.value) < 0) {
+                if (left != null) {
+                    left.add(value);
+                } else {
+                    left = new Node<>(value);
+                }
+            } else {
+                if (right != null) {
+                    right.add(value);
+                } else {
+                    right = new Node<>(value);
+                }
+            }
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public Node<T> getLeftChild() {
+            return left;
+        }
+
+        public Node<T> getRightChild() {
+            return right;
+        }
     }
 }
---
+++ other/src/test/java/BinaryTreeListsTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Tests for the BinaryTreeLists problem
+ */
+public class BinaryTreeListsTest {
+    BinaryTreeLists binaryTreeLists = new BinaryTreeLists();
+
+    @Test
+    public void getPossibleLists() throws Exception {
+
+    }
+}
