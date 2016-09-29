b379f26fdc26dc484adfc8c963e2400d7293dbca
Brian Bamsch <bbamsch32@gmail.com>
2016-09-28 21:52:55 -0700

    Boggle: Add BoggleNode Class

--- other/src/main/java/Boggle.java
+++ other/src/main/java/Boggle.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given a board of letters and a word (or list of words)
  * determine which of the words are present in the board
@@ -17,5 +20,32 @@ public class Boggle {
 
     public boolean wordPresent(String word) {
         return false;
+    }
+
+    private static class BoggleNode<T> {
+        T value;
+
+        List<BoggleNode<T>> neighbors;
+
+        public GraphNode(T value) {
+            this.value = value;
+        }
+
+        public GraphNode(T value, int neighborInitialCapacity) {
+            this(value);
+            this.neighbors = new ArrayList<>(neighborInitialCapacity);
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public List<BoggleNode<T>> getNeighbors() {
+            return neighbors;
+        }
+
+        public void setNeighbors(List<BoggleNode<T>> neighbors) {
+            this.neighbors = neighbors;
+        }
     }
 }
