f2f959f83307aacaaddaed1e2ff2b258fd77e49f
Brian Bamsch <bbamsch32@gmail.com>
2016-09-28 21:02:31 -0700

    Boggle: Add Skeleton

---
+++ other/src/main/java/Boggle.java
@@ -0,0 +1,21 @@
+/**
+ * Given a board of letters and a word (or list of words)
+ * determine which of the words are present in the board
+ */
+public class Boggle {
+    public static int wordsPresent(char[][] gameBoard, String[] words) {
+        return 0;
+    }
+
+    private int wordsPresent(String[] words) {
+        return 0;
+    }
+
+    public static boolean wordPresent(char[][] gameBoard, String word) {
+        return false;
+    }
+
+    private int wordPresent(String word) {
+        return false;
+    }
+}
