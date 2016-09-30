7f8ab0de40ef47c4b766bd4a25e976e23703c06f
Brian Bamsch <bbamsch32@gmail.com>
2016-09-29 22:44:48 -0700

    CoinReversing: Class structure changes

--- topcoder/src/main/java/CoinReversing.java
+++ topcoder/src/main/java/CoinReversing.java
@@ -3,7 +3,24 @@
  * https://community.topcoder.com/stat?c=problem_statement&pm=11473
  */
 public class CoinReversing {
+    private final int n;
+    private final int[] a;
+
+    public CoinReversing(int n, int[] a) {
+        this.n = n;
+        this.a = a;
+    }
+
     public static double expectedHeads(int N, int[] a) {
+        return new CoinReversing(N, a).expectedHeads();
+    }
+
+    private double expectedHeads() {
+        return expectedHeadsInternal(n, 0);
+    }
+
+    private double expectedHeadsInternal(int heads, int index) {
         return 0D;
+    }
     }
 }
