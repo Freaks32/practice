fe2a5f0c4d6b1460ebd62b96e387c6a37749e02a
Brian Bamsch <bbamsch32@gmail.com>
2016-09-29 22:11:46 -0700

    CoinReversing: Add Skeleton

---
+++ topcoder/src/main/java/CoinReversing.java
@@ -0,0 +1,9 @@
+/**
+ * CoinReversing
+ * https://community.topcoder.com/stat?c=problem_statement&pm=11473
+ */
+public class CoinReversing {
+    public static double expectedHeads(int N, int[] a) {
+        return 0D;
+    }
+}
