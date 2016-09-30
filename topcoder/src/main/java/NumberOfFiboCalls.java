4d0acf6314e70c4dc0e57b0cfb764cd38ca9556d
Brian Bamsch <bbamsch32@gmail.com>
2016-09-29 23:27:26 -0700

    NumberOfFiboCalls: Add Caching Helpers

--- topcoder/src/main/java/NumberOfFiboCalls.java
+++ topcoder/src/main/java/NumberOfFiboCalls.java
@@ -1,9 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * NumberofFiboCalls
  * https://community.topcoder.com/stat?c=problem_statement&pm=2292
  */
 public class NumberOfFiboCalls {
+    Map<Integer, Integer[]> callLookup = new HashMap<>();
+
     public int[] fiboCallsMade(int n) {
         return null;
+    }
+
+    private Integer[] getNumberOfCalls(int n) {
+        if (callLookup.containsKey(n)) {
+            return callLookup.get(n);
+        }
+        return null;
+    }
+
+    private void setNumberOfCalls(int n, Integer[] numCalls) {
+        callLookup.put(n, numCalls);
     }
 }
