c5407a6023c1cc4bb37715dc77c62690d32b7ef6
Brian Bamsch <bbamsch32@gmail.com>
2016-09-30 00:13:58 -0700

    EmoticonsDiv2: Add Skeleton

---
+++ topcoder/src/main/java/EmoticonsDiv2.java
@@ -0,0 +1,9 @@
+/**
+ * EmoticonsDiv2
+ * https://community.topcoder.com/stat?c=problem_statement&pm=13041
+ */
+public class EmoticonsDiv2 {
+    public int printSmiles(int smiles) {
+        return 0;
+    }
+}
