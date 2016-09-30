f10f565e7e4f38af2baab140ad6c437859ce23ab
Brian Bamsch <bbamsch32@gmail.com>
2016-09-29 23:53:53 -0700

    BinarySearcahble: Add Caching Mechanism

--- topcoder/src/main/java/BinarySearchable.java
+++ topcoder/src/main/java/BinarySearchable.java
@@ -1,9 +1,39 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * BinarySearchable
  * https://community.topcoder.com/stat?c=problem_statement&pm=5869
  */
 public class BinarySearchable {
+    private final PriorityQueue<Integer> lowQueue;
+    private final PriorityQueue<Integer> highQueue;
+
+    private BinarySearchable(int[] sequence) {
+        lowQueue = new PriorityQueue<>(10, new MaxHeap());
+        highQueue = new PriorityQueue<>(10);
+
+        for (int integer : sequence) {
+            lowQueue.add(integer);
+        }
+    }
+
     public static int howMany(int[] sequence) {
+        return new BinarySearchable(sequence).howMany();
+    }
+
+    private int howMany() {
         return 0;
     }
+
+    /**
+     * Provides MaxHeap functionality for a PriorityQueue
+     */
+    private static class MaxHeap implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    }
+
 }
