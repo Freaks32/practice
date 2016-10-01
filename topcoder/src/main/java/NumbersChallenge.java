af4c98a59983efc5e398b2d2aa8e87cde4391001
Brian Bamsch <bbamsch32@gmail.com>
2016-10-01 13:16:53 -0700

    NumbersChallenge: Add Skeleton

---
+++ topcoder/src/main/java/NumbersChallenge.java
@@ -0,0 +1,9 @@
+/**
+ * NumbersChallenge
+ * https://community.topcoder.com/stat?c=problem_statement&pm=13166
+ */
+public class NumbersChallenge {
+    public int minNumber(int[] s) {
+        return 0;
+    }
+}
