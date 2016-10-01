2c964ad62088abb642406af37c61054ab0dbd494
Brian Bamsch <bbamsch32@gmail.com>
2016-10-01 13:43:08 -0700

    NumbersChallenge: Add internal sorted array

--- topcoder/src/main/java/NumbersChallenge.java
+++ topcoder/src/main/java/NumbersChallenge.java
@@ -1,9 +1,22 @@
+import java.util.Arrays;
+
 /**
  * NumbersChallenge
  * https://community.topcoder.com/stat?c=problem_statement&pm=13166
  */
 public class NumbersChallenge {
-    public int minNumber(int[] s) {
+    int[] a;
+
+    private NumbersChallenge(int[] s) {
+        a = Arrays.copyOf(s, s.length);
+        Arrays.sort(a);
+    }
+
+    public static int minNumber(int[] s) {
+        return new NumbersChallenge(s).minNumber();
+    }
+
+    private int minNumber() {
         return 0;
     }
 }
