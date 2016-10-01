12f333eb324fcd76c3eb0eacb1d3fc795fe931a1
Brian Bamsch <bbamsch32@gmail.com>
2016-10-01 00:35:07 -0700

    EmoticonsDiv2: Add Caching Mechanism

--- topcoder/src/main/java/EmoticonsDiv2.java
+++ topcoder/src/main/java/EmoticonsDiv2.java
@@ -3,7 +3,28 @@
  * https://community.topcoder.com/stat?c=problem_statement&pm=13041
  */
 public class EmoticonsDiv2 {
+    private static final int NO_RESULT_STORED = 0;
+
+    final int numSmiles;
+    final int[][] smileLookup;
+
+    private EmoticonsDiv2(int numSmiles) {
+        this.numSmiles = numSmiles;
+        smileLookup = new int[numSmiles][numSmiles];
+    }
+
     public static int printSmiles(int smiles) {
-        return 0;
+        return new EmoticonsDiv2(smiles).printSmilesInternal(1, 0);
+    }
+
+    private int printSmilesInternal(int currentSmiles, int copiedSmiles) {
+    }
+
+    private void setSmiles(int currentSmiles, int copiedSmiles, int calculatedMoves) {
+        smileLookup[currentSmiles][copiedSmiles] = calculatedMoves;
+    }
+
+    private int getSmiles(int currentSmiles, int copiedSmiles) {
+        return smileLookup[currentSmiles][copiedSmiles];
     }
 }
