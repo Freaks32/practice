684b415ad8c765b26ea86d2968bc858dbfffa367
Brian Bamsch <bbamsch32@gmail.com>
2016-09-30 23:58:34 -0700

    EmoticonsDiv2: Add Test

--- topcoder/src/main/java/EmoticonsDiv2.java
+++ topcoder/src/main/java/EmoticonsDiv2.java
@@ -3,7 +3,7 @@
  * https://community.topcoder.com/stat?c=problem_statement&pm=13041
  */
 public class EmoticonsDiv2 {
-    public int printSmiles(int smiles) {
+    public static int printSmiles(int smiles) {
         return 0;
     }
 }
---
+++ topcoder/src/test/java/EmoticonsDiv2Test.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test for EmoticonsDiv2 Problem
+ */
+public class EmoticonsDiv2Test {
+    @Test
+    public void printSmiles() throws Exception {
+        Assert.assertEquals(2, EmoticonsDiv2.printSmiles(2));
+        Assert.assertEquals(5, EmoticonsDiv2.printSmiles(6));
+        Assert.assertEquals(11, EmoticonsDiv2.printSmiles(11));
+        Assert.assertEquals(8, EmoticonsDiv2.printSmiles(16));
+        Assert.assertEquals(1000, EmoticonsDiv2.printSmiles(1000));
+    }
+}
