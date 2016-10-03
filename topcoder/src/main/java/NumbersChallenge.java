65dc199f9b6b99cf8e944b39995f9ea2f3c6e3a6
Brian Bamsch <bbamsch32@gmail.com>
2016-10-02 22:44:15 -0700

    NumbersChallenge: Implement main functionality

--- topcoder/src/main/java/NumbersChallenge.java
+++ topcoder/src/main/java/NumbersChallenge.java
@@ -1,22 +1,41 @@
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * NumbersChallenge
  * https://community.topcoder.com/stat?c=problem_statement&pm=13166
  */
 public class NumbersChallenge {
-    int[] a;
+    Set<Integer> possibleSums = new HashSet<>();
 
     private NumbersChallenge(int[] s) {
-        a = Arrays.copyOf(s, s.length);
-        Arrays.sort(a);
+        calculateSums(s);
     }
 
+    // Given the bounded-ness of S, (1-20), we will have no more than ~1M total possible sums
+    // This can fit within reasonable time constraint for O(2^N) time & space.
     public static int minNumber(int[] s) {
         return new NumbersChallenge(s).minNumber();
     }
 
     private int minNumber() {
-        return 0;
+        int i = 0;
+        while (possibleSums.contains(i)) {
+            i++;
+        }
+        return i;
+    }
+
+    private void calculateSums(int[] s) {
+        calculateSums(s, 0, 0);
+    }
+
+    private void calculateSums(int[] s, int index, int prefixSum) {
+        if (index < s.length) {
+            calculateSums(s, index + 1, prefixSum);
+            calculateSums(s, index + 1, prefixSum + s[index]);
+        } else {
+            possibleSums.add(prefixSum);
+        }
     }
 }
