70468432bf99fe00e90980609d6faa04ac4cdaef
Brian Bamsch <bbamsch32@gmail.com>
2016-10-02 23:00:45 -0700

    VariableBaseMultiplication: Add Basic Impl (w/ Java built-ins)

--- other/src/main/java/VariableBaseMultiplication.java
+++ other/src/main/java/VariableBaseMultiplication.java
@@ -5,6 +5,9 @@
  */
 public class VariableBaseMultiplication {
     public static String multiply(String a, String b, int base) {
-        return null;
+        long aBase10 = Long.parseLong(a, base);
+        long bBase10 = Long.parseLong(b, base);
+
+        return Long.toString(aBase10 * bBase10, base);
     }
 }
