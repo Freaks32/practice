04c1b317f7120ce8d39f935289d45026f8b07cb7
Brian Bamsch <bbamsch32@gmail.com>
2016-10-02 23:00:02 -0700

    VariableBaseMultiplication: Add Skeleton

---
+++ other/src/main/java/VariableBaseMultiplication.java
@@ -0,0 +1,10 @@
+/**
+ * Variable Base Multiplication
+ * <p>
+ * Multiply two numbers given the base of those numbers
+ */
+public class VariableBaseMultiplication {
+    public static String multiply(String a, String b, int base) {
+        return null;
+    }
+}
