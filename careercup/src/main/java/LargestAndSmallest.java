fb98b67d2851a8e7011dd810148426e094d67450
Brian Bamsch <bbamsch32@gmail.com>
2016-10-03 21:10:57 -0700

    LargestAndSmallest: Skeleton

---
+++ careercup/src/main/java/LargestAndSmallest.java
@@ -0,0 +1,18 @@
+/**
+ * https://careercup.com/question?id=5737503739871232
+ * <p>
+ * Find the largest and smallest number in a list. The list is stored as two sections, one in ascending order and the other in descending order.
+ * <p>
+ * input [ 2 3 4 5 6 7 10 9 8 7]
+ * smallest : 2
+ * Largest 10
+ */
+public class LargestAndSmallest {
+    public static int smallest(int[] array) {
+        return 0;
+    }
+
+    public static int largest(int[] array) {
+        return 0;
+    }
+}
