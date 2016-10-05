c27489c17f224c0d85321e5a893543171bfc2deb
Brian Bamsch <bbamsch32@gmail.com>
2016-10-04 20:28:52 -0700

    BoxDock: Add Skeleton

---
+++ careercup/src/main/java/BoxDock.java
@@ -0,0 +1,15 @@
+/**
+ * https://careercup.com/question?id=5173875873677312
+ * <p>
+ * There are n+1 loading docks.
+ * a permutation of boxes 1->n is placed on the first n
+ * there is a fork that can move one box to an empty location at a time.
+ * Give an algorithm to sort then boxes with minimum number of moves.
+ * <p>
+ * Follow up: minimum distance
+ */
+public class BoxDock {
+    public static int sortBoxes(int[] docks) {
+        return 0;
+    }
+}
