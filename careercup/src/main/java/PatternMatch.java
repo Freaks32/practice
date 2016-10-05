bf371d4951b1ba6ef4e95ee662974c5b2011837e
Brian Bamsch <bbamsch32@gmail.com>
2016-10-04 23:37:23 -0700

    PatternMatch: Add Implementation

--- careercup/src/main/java/PatternMatch.java
+++ careercup/src/main/java/PatternMatch.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 /**
  * https://careercup.com/question?id=5704645247762432
@@ -11,6 +11,46 @@ import java.util.List;
  */
 public class PatternMatch {
     public static List<String> matchPattern(String pattern, String[] strings) {
-        return null;
+        List<String> output = new ArrayList<>();
+        char[] pat = pattern.toCharArray();
+
+        for (String string : strings) {
+            Map<Character, Character> mapping = new HashMap<>();
+            Set<Character> mappedOutputs = new HashSet<>();
+
+            char[] characters = string.toCharArray();
+            boolean match = true;
+
+            if (characters.length != pat.length) {
+                continue;
+            }
+
+            for (int i = 0; i < pat.length && match; i++) {
+                char p = pat[i];
+                char c = characters[i];
+
+                if (mapping.containsKey(p)) {
+                    if (!mapping.get(p).equals(c)) {
+                        // Current pattern to string mapping not met
+                        match = false;
+                    }
+                } else {
+                    if (mappedOutputs.contains(c)) {
+                        // Character in the output string is already mapped to another input character
+                        match = false;
+                    } else {
+                        // New mapping, add it
+                        mapping.put(p, c);
+                        mappedOutputs.add(c);
+                    }
+                }
+            }
+
+            if (match) {
+                output.add(string);
+            }
+        }
+        
+        return output;
     }
 }
