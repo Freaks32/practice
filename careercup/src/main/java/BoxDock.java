bf4d4746e18f4b9eeb3831e0947288f2b1eb8552
Brian Bamsch <bbamsch32@gmail.com>
2016-10-04 20:37:45 -0700

    BoxDock: Add Initialization

--- careercup/src/main/java/BoxDock.java
+++ careercup/src/main/java/BoxDock.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * https://careercup.com/question?id=5173875873677312
  * <p>
@@ -9,7 +11,31 @@
  * Follow up: minimum distance
  */
 public class BoxDock {
+    private static final int NO_BOX = -1;
+
+    private int numMoves = 0;
+    private int[] docks;
+    private int[] boxLookup;
+
+    private BoxDock(int[] docks) {
+        this.docks = Arrays.copyOf(docks, docks.length + 1);
+        this.docks[this.docks.length - 1] = NO_BOX;
+
+        this.boxLookup = new int[docks.length];
+        for (int i = 0; i < docks.length; i++) {
+            boxLookup[docks[i] - 1] = i;
+        }
+    }
+
     public static int sortBoxes(int[] docks) {
+        return new BoxDock(docks).sortBoxes();
+    }
+
+    private int sortBoxes() {
         return 0;
+    }
+
+    private int getBoxIndex(int boxNum) {
+        return boxLookup[boxNum - 1];
     }
 }
