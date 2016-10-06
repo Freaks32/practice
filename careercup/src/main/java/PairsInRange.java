0ef32e544425f3d60db163d51431ff73804c6a57
Brian Bamsch <bbamsch32@gmail.com>
2016-10-05 22:57:04 -0700

    PairsInRange: Add Skeleton

---
+++ careercup/src/main/java/PairsInRange.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+
+/**
+ * Pairs Sum to Range of Values
+ * <p>
+ * Write a function that takes as input an array of integers A, and two integers low and high.
+ * <p>
+ * Your function has to output pairs of indices: {(i,j), ...}
+ * <p>
+ * Where each pair of indices denotes that the subarray of A[i...j] has a sum in the range low <= sum <= high.
+ */
+public class PairsInRange {
+    public static Set<Pair<Integer>> pairsInRange(int[] numbers, int low, int high) {
+        return null;
+    }
+}
