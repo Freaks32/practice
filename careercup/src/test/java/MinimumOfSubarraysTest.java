4f4111634e2ee958d03cda89d23fb504151fc13f
Brian Bamsch <bbamsch32@gmail.com>
2016-10-05 23:35:23 -0700

    MinimumOfSubarraysTest: Add Additional Tests

--- careercup/src/test/java/MinimumOfSubarraysTest.java
+++ careercup/src/test/java/MinimumOfSubarraysTest.java
@@ -10,5 +10,11 @@ public class MinimumOfSubarraysTest {
         Assert.assertArrayEquals(new Integer[]{5, 29, 4, 2, 1, 1, 1, 1},
                 MinimumOfSubarrays.minimumOfSubarrays(
                         new Integer[]{5, 50, 52, 30, 29, 4, 2, 1, 2, 2, 4}, 4));
+        Assert.assertArrayEquals(new Integer[]{1, 1},
+                MinimumOfSubarrays.minimumOfSubarrays(
+                        new Integer[]{10, 3, 5, 1, 4, 7}, 5));
+        Assert.assertArrayEquals(new Integer[]{23, 31, 31, 38},
+                MinimumOfSubarrays.minimumOfSubarrays(
+                        new Integer[]{23, 32, 31, 38, 40, 40, 40, 40}, 5));
     }
 }
