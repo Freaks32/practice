b629fafa0e939c096f9af0e5a8e243fa9e837225
Brian Bamsch <bbamsch32@gmail.com>
2016-10-05 22:57:33 -0700

    PairsInRange: Add Binary Search helper function

--- careercup/src/main/java/PairsInRange.java
+++ careercup/src/main/java/PairsInRange.java
@@ -13,4 +13,34 @@ public class PairsInRange {
     public static Set<Pair<Integer>> pairsInRange(int[] numbers, int low, int high) {
         return null;
     }
+
+    /**
+     * Binary search on sorted integer array for a specific value.
+     * Returns the index of the last element matching 'find' or the last element
+     * less than element 'find'
+     * <p>
+     * Weak point: If all numbers in the array are equal, time complexity becomes O(n)
+     * rather than the expected O(lg(n)) for lookup. This can be combated by doing binary
+     * search until the specific index is found, rather than falling back to scanning
+     */
+    private static int binarySearchGte(int[] nums, int find, int start, int end) {
+        if (start >= end) {
+            return start;
+        }
+
+        int mid = (end + start) / 2;
+        int val = nums[mid];
+
+        if (val < find) {
+            return binarySearchGte(nums, find, mid + 1, end);
+        } else if (val > find) {
+            return binarySearchGte(nums, find, start, mid - 1);
+        } else {
+            int index = mid;
+            while (index - 1 >= 0 && nums[index - 1] == find) {
+                index--;
+            }
+            return index;
+        }
+    }
 }
