4cefaf57ed0df2669c172ba44e98156b6efcfb02
Brian Bamsch <bbamsch32@gmail.com>
2016-10-06 23:07:14 -0700

    LongestPalindrome: Add Implementation

--- careercup/src/main/java/LongestPalindrome.java
+++ careercup/src/main/java/LongestPalindrome.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Longest Panindrome
  * https://careercup.com/question?id=5631060781039616
@@ -14,6 +17,38 @@
  */
 public class LongestPalindrome {
     public static String longestPalindrome(String s) {
-        return null;
+        Map<Character, Integer> characterCount = new HashMap<>();
+        char[] characters = s.toCharArray();
+
+        for (char c : characters) {
+            if (!characterCount.containsKey(c)) {
+                characterCount.put(c, 1);
+            } else {
+                characterCount.put(c, characterCount.get(c) + 1);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Character oddCharacter = null;
+        for (Character c : characterCount.keySet()) {
+            Integer count = characterCount.get(c);
+            if (count % 2 != 0) {
+                oddCharacter = c;
+            }
+            for (int i = 0; i < count / 2; i++) {
+                sb.append(c);
+            }
+        }
+
+        String firstHalf = sb.toString();
+        if (oddCharacter != null) {
+            sb.append(oddCharacter);
+        }
+
+        for (int i = firstHalf.length() - 1; i >= 0; i--) {
+            sb.append(firstHalf.charAt(i));
+        }
+
+        return sb.toString();
     }
 }
