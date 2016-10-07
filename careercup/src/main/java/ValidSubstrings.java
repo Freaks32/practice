86ea9d4bc726e1e26349c5a16e198d86a4d36124
Brian Bamsch <bbamsch32@gmail.com>
2016-10-06 21:51:27 -0700

    ValidSubstrings: Add Skeleton

---
+++ careercup/src/main/java/ValidSubstrings.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+/**
+ * ValidSubstrings
+ * <p>
+ * Given a dictionary and a string, find all the substrings that are valid words in dictionary.
+ */
+public class ValidSubstrings {
+    public static List<String> getValidSubstrings(String[] dictionary, String input) {
+        return null;
+    }
+}
