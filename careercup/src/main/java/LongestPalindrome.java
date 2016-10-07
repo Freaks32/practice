a6e968e7ef4e405f4e22416878dba3077f84436f
Brian Bamsch <bbamsch32@gmail.com>
2016-10-06 22:55:12 -0700

    LongestPalindrome: Add Skeleton

---
+++ careercup/src/main/java/LongestPalindrome.java
@@ -0,0 +1,19 @@
+/**
+ * Longest Panindrome
+ * https://careercup.com/question?id=5631060781039616
+ * <p>
+ * Given a string return the longest palindrome that can be constructed by removing or shuffling characters.
+ * <p>
+ * Example:
+ * 'aha' -> 'aha'
+ * 'ttaatta' -> ' ttaaatt'
+ * 'abc' -> 'a' or 'b' or 'c'
+ * 'gggaaa' -> 'gaaag' or 'aggga'
+ * <p>
+ * Note if there are multiple correct answers you only need to return 1 palindrome.
+ */
+public class LongestPalindrome {
+    public static String longestPalindrome(String s) {
+        return null;
+    }
+}
