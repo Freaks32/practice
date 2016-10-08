2686f7e1e4590a716e2b679084cb9ef8ba3bfdce
Brian Bamsch <bbamsch32@gmail.com>
2016-10-07 22:18:23 -0700

    WordFit: Add additional test

--- careercup/src/test/java/WordFitTest.java
+++ careercup/src/test/java/WordFitTest.java
@@ -10,5 +10,6 @@ public class WordFitTest {
         Assert.assertEquals(5, WordFit.numberOfWords(new String[]{"Do", "Run"}, 9, 2));
         Assert.assertEquals(16, WordFit.numberOfWords(new String[]{"Jane", "Doe"}, 8, 8));
         Assert.assertEquals(10, WordFit.numberOfWords(new String[]{"asdf", "jkl", "qwerty", "to"}, 14, 4));
+        Assert.assertEquals(2, WordFit.numberOfWords(new String[]{"hello", "world", "thiswillnotfit"}, 10, 10));
     }
 }
