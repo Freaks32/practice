204fd25cd3eb9d619b2f454a185c9fd5fa68ff39
Brian Bamsch <bbamsch32@gmail.com>
2016-10-07 22:00:44 -0700

    WordFit: Add Skeleton

---
+++ careercup/src/main/java/WordFit.java
@@ -0,0 +1,20 @@
+/**
+ * WordFit
+ * https://careercup.com/question?id=5701279419465728
+ * <p>
+ * GIven a list of words, and the number of rows and columns, return the number of words that can be fit into the rows and columns by stringing together each consecutive word. If the next word doesn't fit in the same line, it should move to the next line. Find an efficient solution for this. For eg.
+ * <p>
+ * List of words: { "Do", "Run" }
+ * Number of columns: 9
+ * Number of rows: 2
+ * <p>
+ * First row: "Do Run Do" (7 letters + 2 spaces fit into 9 columns)
+ * Second row: "Run Do" (Only 2 words fit into 9 columns)
+ * <p>
+ * The solution here would be 5
+ */
+public class WordFit {
+    public static int numberOfWords(String[] words, int columns, int rows) {
+        return 0;
+    }
+}
