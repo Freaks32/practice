164d62dc9f8a906c26226adc871a19f2f6783dc3
Brian Bamsch <bbamsch32@gmail.com>
2016-10-10 21:44:44 -0700

    MatrixPaths: Add Implementation

--- careercup/src/main/java/MatrixPaths.java
+++ careercup/src/main/java/MatrixPaths.java
@@ -9,6 +9,35 @@
  */
 public class MatrixPaths {
     public static long numPaths(boolean[][] map) {
-        return 0L;
+        if (map.length < 1 || map[0].length < 1) {
+            throw new IllegalArgumentException("Map is Invalid");
+        }
+
+        // Map to count number of paths to a given point
+        long[][] pathMap = new long[map.length][map[0].length];
+
+        // Set up Boundary Conditions
+        pathMap[0][0] = 1;
+        int index = 1;
+        while (index < map[0].length && map[0][index]) {
+            pathMap[0][index] = pathMap[0][index - 1];
+            index++;
+        }
+        index = 1;
+        while (index < map.length && map[index][0]) {
+            pathMap[index][0] = pathMap[index - 1][0];
+            index++;
+        }
+
+        // Fill in Number of Paths
+        for (int i = 1; i < map.length; i++) {
+            for (int j = 1; j < map[i].length; j++) {
+                if (map[i][j]) {
+                    pathMap[i][j] = pathMap[i - 1][j] + pathMap[i][j - 1];
+                }
+            }
+        }
+
+        return pathMap[pathMap.length - 1][pathMap[0].length - 1];
     }
 }
