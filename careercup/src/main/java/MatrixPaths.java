c44ce9c5a70faee724989f8f573ed0b1181b4d1d
Brian Bamsch <bbamsch32@gmail.com>
2016-10-10 21:28:23 -0700

    MatrixPaths: Add Skeleton

---
+++ careercup/src/main/java/MatrixPaths.java
@@ -0,0 +1,14 @@
+/**
+ * MatrixPaths Problem
+ * https://www.careercup.com/question?id=8248228
+ * <p>
+ * Given a MxN matrix, find the total number of possible paths from top-left to bottom-right element,
+ * you can go rightwards and downwards only. Now, assume some of the entries in the matrix are blocked,
+ * find the number of such paths. For example: For a 3X3 matrix, total number of paths is 6.
+ * For second case, if we block entry (2,2), we have only 2 paths available.
+ */
+public class MatrixPaths {
+    public static long numPaths(boolean[][] map) {
+        return 0L;
+    }
+}
