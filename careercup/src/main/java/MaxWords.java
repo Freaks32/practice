ade22102819687d85d835602c28a1b0883e40d59
Brian Bamsch <bbamsch32@gmail.com>
2016-10-11 19:43:17 -0700

    MaxWords: Add Skeleton

---
+++ careercup/src/main/java/MaxWords.java
@@ -0,0 +1,28 @@
+/**
+ * Max Words Problem
+ * https://careercup.com/question?id=5639859738771456
+ * <p>
+ * Let's say I have a word "I love chicken", I can break the number of characters in each word, like so: [1] [4] [7]
+ * [1,4] [4,7], [1,4,7].
+ * <p>
+ * Now let's say I have a max = 5. The phrases with equal or fever than 5 characters are "[I], [love], and [I, love]. The longest phrase is [I,love], which contains 2 words.
+ * <p>
+ * Complete the Length function given. It has 2 parameters:
+ * <p>
+ * 1) An array of integers, named array
+ * 2) A maximum number, named max.
+ * <p>
+ * int maxWords(int [] array, int max)
+ * <p>
+ * Example test case 1:
+ * <p>
+ * [3,1,2,3]
+ * 4
+ * <p>
+ * Output expected : 2
+ */
+public class MaxWords {
+    public static int maxWords(int[] array, int max) {
+        return 0;
+    }
+}
