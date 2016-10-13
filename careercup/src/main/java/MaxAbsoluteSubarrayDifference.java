bf5da70a44adaf2d7a21023365a30acbaa985402
Brian Bamsch <bbamsch32@gmail.com>
2016-10-12 21:10:04 -0700

    MaxAbsoluteSubarrayDifference: Add Caching Helper Class

--- careercup/src/main/java/MaxAbsoluteSubarrayDifference.java
+++ careercup/src/main/java/MaxAbsoluteSubarrayDifference.java
@@ -8,7 +8,22 @@
  * eg- [2 -1 -2 1 -4 2 8] ans - (-1 -2 1 -4) (2 8), diff = 16
  */
 public class MaxAbsoluteSubarrayDifference {
+    private static class Helper {
+        private final int[] array;
+
+        private final int[][] subarraySum;
+
+        public Helper(int[] array) {
+            this.array = array;
+            this.subarraySum = new int[array.length][array.length];
+        }
+
+        public int maxAbsoluteSubarrayDifference() {
+            return 0;
+        }
+    }
+
     public static int maxAbsoluteSubarrayDifference(int[] array) {
-        return 0;
+        return new Helper(array).maxAbsoluteSubarrayDifference();
     }
 }
