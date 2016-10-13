81cd73532f2f161f5c9dfefe64892b9f3772f0f8
Brian Bamsch <bbamsch32@gmail.com>
2016-10-12 20:43:48 -0700

    MaxAbsoluteSubarrayDifference: Add Test

---
+++ careercup/src/test/java/MaxAbsoluteSubarrayDifferenceTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test for MaxAbsoluteSubarrayDifference Problem
+ */
+public class MaxAbsoluteSubarrayDifferenceTest {
+    @Test
+    public void maxAbsoluteSubarrayDifference() throws Exception {
+        Assert.assertEquals(16, MaxAbsoluteSubarrayDifference.maxAbsoluteSubarrayDifference(
+                new int[]{2, -1, -2, -4, 2, 8}
+        ));
+        Assert.assertEquals(10, MaxAbsoluteSubarrayDifference.maxAbsoluteSubarrayDifference(
+                new int[]{4, -1, 7}
+        ));
+        Assert.assertEquals(50, MaxAbsoluteSubarrayDifference.maxAbsoluteSubarrayDifference(
+                new int[]{-5, -5, 10, 10, 10, 4, 2, 1, -1, -7, -5, 5}
+        ));
+    }
+}
