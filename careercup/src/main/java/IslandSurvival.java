db63012fe88c0ed131c1dd66b293fd39c859babd
Brian Bamsch <bbamsch32@gmail.com>
2016-10-12 22:06:49 -0700

    IslandSurvival: Add Skeleton

---
+++ careercup/src/main/java/IslandSurvival.java
@@ -0,0 +1,22 @@
+/**
+ * IslandSurvival
+ * https://careercup.com/question?id=15556758
+ * <p>
+ * There is an island which is represented by square matrix NxN.
+ * A person on the island is standing at any given co-ordinates (x,y).
+ * He can move in any direction one step right, left, up, down on the island.
+ * If he steps outside the island, he dies.
+ * <p>
+ * Let island is represented as (0,0) to (N-1,N-1) (i.e NxN matrix) & person
+ * is standing at given co-ordinates (x,y).
+ * He is allowed to move n steps on the island (along the matrix).
+ * What is the probability that he is alive after he walks n steps on the island?
+ * <p>
+ * Write a psuedocode & then full code for function
+ * float probabilityofalive(int x,int y, int n)
+ */
+public class IslandSurvival {
+    public static double probabilityAlive(int x, int y, int n) {
+        return 0D;
+    }
+}
