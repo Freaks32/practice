c756c535b983ec2ae7677238a9e72943da12b2c3
Brian Bamsch <bbamsch32@gmail.com>
2016-10-13 21:48:27 -0700

    GuardDistance: Refactor + Add Implementation
    Refactor to use a Helper Class to avoid passing around map & queue
    between functions.
    Add Implementation to problem.

--- careercup/src/main/java/GuardDistance.java
+++ careercup/src/main/java/GuardDistance.java
@@ -23,21 +23,71 @@ public class GuardDistance {
     public static final int GUARD = -1;
     public static final int UNMARKED = 0;
 
+    /**
+     * Assume input array can be modified
+     * <p>
+     * Do BFS starting from all Guard positions to determine distance to each
+     * point from closest guard. Because we use BFS, the first time a square
+     * is encountered is the distance from the closest guard
+     */
     public static int[][] minGuardDistances(int[][] array) {
-        Queue<Position> positionQueue = getGuardQueue(array);
-        return null;
+        return new GuardDistanceHelper(array).minGuardDistances();
     }
 
-    private static Queue<Position> getGuardQueue(int[][] array) {
-        Queue<Position> guardPositions = new LinkedList<>();
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (array[i][j] == GUARD) {
-                    guardPositions.add(new Position(i, j));
+    private static class GuardDistanceHelper {
+        private final Queue<Position> positionQueue = new LinkedList<>();
+        private final int[][] map;
+
+        public GuardDistanceHelper(int[][] map) {
+            this.map = map;
+        }
+
+        public int[][] minGuardDistances() {
+            initGuardQueue();
+
+            while (!positionQueue.isEmpty()) {
+                Position pos = positionQueue.poll();
+                int x = pos.getX();
+                int y = pos.getY();
+                int nextDistance = map[x][y] + 1;
+                if (map[x][y] == GUARD) {
+                    nextDistance = 1;
+                }
+                touchPosition(x - 1, y, nextDistance);
+                touchPosition(x + 1, y, nextDistance);
+                touchPosition(x, y - 1, nextDistance);
+                touchPosition(x, y + 1, nextDistance);
+            }
+
+            return map;
+        }
+
+        private void touchPosition(int x, int y, int distance) {
+            if (boundsCheck(x, y) && map[x][y] == UNMARKED) {
+                map[x][y] = distance;
+                positionQueue.add(new Position(x, y));
+            }
+        }
+
+        private boolean boundsCheck(int x, int y) {
+            if (x >= 0 && x < map.length && y >= 0 && y < map[x].length) {
+                return true;
+            }
+            return false;
+        }
+
+        private void initGuardQueue() {
+            if (positionQueue.size() != 0) {
+                throw new IllegalArgumentException("Position Queue already Initialized");
+            }
+            for (int i = 0; i < map.length; i++) {
+                for (int j = 0; j < map[i].length; j++) {
+                    if (map[i][j] == GUARD) {
+                        positionQueue.add(new Position(i, j));
+                    }
                 }
             }
         }
-        return guardPositions;
     }
 
     private static class Position {
