de603cc9f1af89ebdb086fffd898db0a7c43bb49
Brian Bamsch <bbamsch32@gmail.com>
2016-10-13 21:20:10 -0700

    GuardDistance: Add Constants

--- careercup/src/main/java/GuardDistance.java
+++ careercup/src/main/java/GuardDistance.java
@@ -16,6 +16,10 @@
  * Write the algorithm, with optimal solution.
  */
 public class GuardDistance {
+    private static final int OBSTACLE = -2;
+    private static final int GUARD = -1;
+    private static final int UNMARKED = 0;
+
     public static int[][] minGuardDistances(int[][] array) {
         return null;
     }
