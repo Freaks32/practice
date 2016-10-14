31582e98a4201d59952fffd94f842c106b248192
Brian Bamsch <bbamsch32@gmail.com>
2016-10-13 22:32:21 -0700

    SpiralMatrix: Add Skeleton

---
+++ leetcode/src/main/java/SpiralMatrix.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+/**
+ * SprialMatrix
+ * https://leetcode.com/problems/spiral-matrix/
+ * <p>
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ * <p>
+ * For example,
+ * Given the following matrix:
+ * <p>
+ * [
+ * [ 1, 2, 3 ],
+ * [ 4, 5, 6 ],
+ * [ 7, 8, 9 ]
+ * ]
+ * You should return [1,2,3,6,9,8,7,4,5].
+ */
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        return null;
+    }
+}
