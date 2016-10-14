be1c04c26204821bf0de4cbea2e29ad3e79849dc
Brian Bamsch <bbamsch32@gmail.com>
2016-10-13 21:19:05 -0700

    GuardDistance: Add Skeleton

---
+++ careercup/src/main/java/GuardDistance.java
@@ -0,0 +1,22 @@
+/**
+ * GuardDistance
+ * https://careercup.com/question?id=4716965625069568
+ * <p>
+ * Given a 2-D matrix represents the room, obstacle and guard like
+ * the following (0 is room, B->obstacle, G-> Guard):
+ * 0 0 0
+ * B G G
+ * B 0 0
+ * <p>
+ * calculate the steps from a room to nearest Guard
+ * and set the matrix, like this
+ * 2 1 1
+ * B G G
+ * B 1 1
+ * Write the algorithm, with optimal solution.
+ */
+public class GuardDistance {
+    public static int[][] minGuardDistances(int[][] array) {
+        return null;
+    }
+}
