b0afe72e8668fb6d4531f74b922921165a16b9dd
Brian Bamsch <bbamsch32@gmail.com>
2016-10-14 22:46:39 -0700

    AggregatedNumber: Add Additional Tests

--- careercup/src/test/java/AggregatedNumberTest.java
+++ careercup/src/test/java/AggregatedNumberTest.java
@@ -11,5 +11,9 @@ public class AggregatedNumberTest {
         Assert.assertTrue(AggregatedNumber.isAggregatedNumber("122436"));
         Assert.assertTrue(AggregatedNumber.isAggregatedNumber("1299111210"));
         Assert.assertTrue(AggregatedNumber.isAggregatedNumber("112112224"));
+        Assert.assertFalse(AggregatedNumber.isAggregatedNumber("112357"));
+        Assert.assertFalse(AggregatedNumber.isAggregatedNumber("1123589"));
+        Assert.assertFalse(AggregatedNumber.isAggregatedNumber("0101"));
+        Assert.assertFalse(AggregatedNumber.isAggregatedNumber("123509"));
     }
 }
