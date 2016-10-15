0930d7743b314dca86c6f6002661e1327e0592de
Brian Bamsch <bbamsch32@gmail.com>
2016-10-14 22:18:26 -0700

    AggregatedNumber: Add Skeleton

---
+++ careercup/src/main/java/AggregatedNumber.java
@@ -0,0 +1,22 @@
+/**
+ * AggregatedNumber
+ * https://careercup.com/question?id=14948278
+ * <p>
+ * we will name a number "aggregated number" if this number
+ * has the following attribute (just like the Fibonacci numbers):
+ * 1,1,2,3,5,8,13.....
+ * <p>
+ * the digits in the number can divided into several parts,
+ * and the later part is the sum of the former parts.
+ * <p>
+ * like 112358, because 1+1=2, 1+2=3, 2+3=5, 3+5=8
+ * 122436, because 12+24=36
+ * 1299111210, because 12+99=111, 99+111=210
+ * 112112224, because 112+112=224
+ * so can you provide a function to check whether this number is aggregated number or not?
+ */
+public class AggregatedNumber {
+    public boolean isAggregatedNumber(String input) {
+        return false;
+    }
+}
