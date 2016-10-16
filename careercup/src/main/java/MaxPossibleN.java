de467738023c7f0f1457e2b24767188d6acbb142
Brian Bamsch <bbamsch32@gmail.com>
2016-10-15 22:48:04 -0700

    MaxPossibleN: Add Skeleton

---
+++ careercup/src/main/java/MaxPossibleN.java
@@ -0,0 +1,16 @@
+/**
+ * MaxPossibleN
+ * https://careercup.com/question?id=5094709806497792
+ * <p>
+ * Given an unsorted array of integers, you need to return maximum possible n such that the array consists at least n values greater than or equals to n. Array can contain duplicate values.
+ * Sample input : [1, 2, 3, 4] -- output : 2
+ * Sample input : [900, 2, 901, 3, 1000] -- output: 3
+ * <p>
+ * - abc June 20, 2014 in India | Report Duplicate | Flag
+ * Google Applications Developer
+ */
+public class MaxPossibleN {
+    public int maxPossibleN(int[] array) {
+        return 0;
+    }
+}
