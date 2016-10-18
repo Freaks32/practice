f50263e0ebd81ca8685039fb3732012dbfc79be3
Brian Bamsch <bbamsch32@gmail.com>
2016-10-17 21:00:40 -0700

    LampGrid: Add Helper Class

--- careercup/src/main/java/LampGrid.java
+++ careercup/src/main/java/LampGrid.java
@@ -16,6 +16,46 @@ public class LampGrid {
     public static boolean[] squaresLit(int N, Coord[] lamps, Coord[] locations) {
         return null;
     }
+
+    private static class LampGridHelper {
+        private final int N;
+        private final boolean[] x;
+        private final boolean[] y;
+        private final boolean[] diag1;
+        private final boolean[] diag2;
+
+        public LampGridHelper(int N, Coord[] lamps) {
+            this.N = N;
+            this.x = new boolean[N];
+            this.y = new boolean[N];
+            this.diag1 = new boolean[(2 * N) - 1];
+            this.diag2 = new boolean[(2 * N) - 1];
+
+            for (Coord lampCoord : lamps) {
+                addLamp(lampCoord);
+            }
+        }
+
+        private int diag1Transform(Coord in) {
+            return (in.getX() - in.getY()) + (N - 1);
+        }
+
+        private int diag2Transform(Coord in) {
+            return (in.getX() + in.getY());
+        }
+
+        private void addLamp(Coord lampCoord) {
+            x[lampCoord.getX()] = true;
+            y[lampCoord.getY()] = true;
+            diag1[diag1Transform(lampCoord)] = true;
+            diag2[diag2Transform(lampCoord)] = true;
+        }
+
+        public boolean isCoordinateLit(Coord input) {
+            return x[input.getX()] || y[input.getY()] ||
+                    diag1[diag1Transform(input)] || diag2[diag2Transform(input)];
+        }
+    }
 }
 
 class Coord {
